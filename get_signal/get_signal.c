05670ad107ee21d8de6ddab23936d8b45b777bd5
TufanKurukaya <kurukayatufan@gmail.com>
2024-10-13 15:56:38 +0300

    Upgrade Makefie and Add minilibx-linux

--- get_signal/get_signal.c
+++ get_signal/get_signal.c
@@ -1,4 +1,13 @@
-#include "minilibx/mlx.h"
+#ifndef MAC_OS
+#define MAC_OS 1
+#endif
+
+#if MAC_OS
+#include "minilibx-macos/mlx.h"
+#else
+#include "minilibx-linux/mlx.h"
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,12 +22,12 @@ const int window_height = 375;
 
 int key_press(int keycode, void *param)
 {
-    char str[12];
+    char str[24];
     sprintf(str, "Signal: %d", keycode);
     mlx_clear_window(mlx_ptr, win_ptr);
     mlx_put_image_to_window(mlx_ptr, win_ptr, background_ptr, 0, 0);
     mlx_put_image_to_window(mlx_ptr, win_ptr, logo_ptr, 20, 10);
-    mlx_string_put(mlx_ptr, win_ptr, (window_width / 2) - 40 , window_height / 2, color, str);
+    mlx_string_put(mlx_ptr, win_ptr, (window_width / 2) - 40, window_height / 2, color, str);
     return (0);
 }
 
@@ -33,14 +42,18 @@ int main()
     int img_width, img_height;
 
     mlx_ptr = mlx_init();
+    if (!mlx_ptr)
+        return (perror("Error"), 1);
     win_ptr = mlx_new_window(mlx_ptr, window_width, window_height, "Signal Window");
+    if (!win_ptr)
+        return (perror("Error"), 1);
     background_ptr = mlx_xpm_file_to_image(mlx_ptr, "./xpm/background.xpm", &img_width, &img_height);
     logo_ptr = mlx_xpm_file_to_image(mlx_ptr, "./xpm/logo.xpm", &img_width, &img_height);
 
     mlx_put_image_to_window(mlx_ptr, win_ptr, background_ptr, 0, 0);
     mlx_put_image_to_window(mlx_ptr, win_ptr, logo_ptr, 20, 10);
     mlx_key_hook(win_ptr, key_press, mlx_ptr);
-    mlx_hook(win_ptr, 17, 1L<<17, close_window, mlx_ptr);
+    mlx_hook(win_ptr, 17, 1L << 17, close_window, mlx_ptr);
 
     mlx_loop(mlx_ptr);
     return (0);
